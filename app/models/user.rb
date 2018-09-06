9cfbbb7fb9346642655d69f6bbd853476fe01e02
jfortega35 <ec2-user@ip-172-31-1-111.us-east-2.compute.internal>
2018-09-06 23:45:46 +0000

    Model associations via AWS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :receipts
+  belongs_to :group
   validates :username, presence: true,
                     length: { minimum: 5 }
+                    
 end
